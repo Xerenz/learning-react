4ac0d076d36953a81e2951cac2097804080849ba
Xerenz <martingeo15@gmail.com>
2020-06-14 12:13:54 +0530

    title component

--- e-commerce/src/components/Navbar.js
+++ e-commerce/src/components/Navbar.js
@@ -7,7 +7,7 @@ import logo from '../logo.svg';
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm navbar-dark px-sm">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm">
                 {/* 
                 https://www.iconfinder.com/icons/1243689/call_phone_icon
                 Creative Commons (Attribution 3.0 Unported);
@@ -26,7 +26,7 @@ export default class Navbar extends Component {
                         <i className="fa fa-cart-plus"> my cart</i>
                     </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         )
     }
 }
@@ -35,15 +35,24 @@ const ButtonContainer = styled.button`
     text-transform : capitalize;
     font-size : 1.4rem;
     background : transparent;
-    border : 0.05rem solid var(--lightBlue);
-    color : var(--lightBlue);
+    border : 0.05rem solid var(--mainYellow);
+    color : var(--mainYellow);
     border-radius : 0.5rem;
     padding : 0.2rem 0.5rem;
     cursor : pointer;
     margin : 0.2rem 0.5rem 0.2rem 0;
     transition : all 0.5s ease-in-and-out;
     &:hover {
-        background : var(--lightBlue);
-        color : var(--mainBlue);
+        background : var(--mainYellow);
+        color : var(--mainWhite);
+    }
+`
+
+const NavWrapper = styled.nav`
+    background : var(--mainBlue);
+    .nav-link {
+        color : var(--mainWhite) !important;
+        font-size : 1.3rem;
+        text-transform : capitalize;
     }
 `
--- e-commerce/src/components/ProductList.js
+++ e-commerce/src/components/ProductList.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import Product from './Product';
+import Title from "./Title";
+
+import {storeProducts} from '../data';
 
 export default class ProductList extends Component {
+    state = {
+        products : storeProducts
+    }
+
     render() {
         return (
-            <div>
-                <Product />
-            </div>
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name="our" title="product"></Title>
+                        <div className="row"/>
+                        <Product/>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
