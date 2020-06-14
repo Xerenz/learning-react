20485679ce5b85d1edb86db53827502d7dd57f40
Xerenz <martingeo15@gmail.com>
2020-06-14 11:27:18 +0530

    styled components

--- e-commerce/src/components/Navbar.js
+++ e-commerce/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
 
 import logo from '../logo.svg';
 
@@ -21,11 +22,28 @@ export default class Navbar extends Component {
                     </li>
                 </ul>
                 <Link to="/cart" className="ml-auto">
-                    <button>
-                        <i className="fa fa-cart-plus"></i>
-                    </button>
+                    <ButtonContainer>
+                        <i className="fa fa-cart-plus"> my cart</i>
+                    </ButtonContainer>
                 </Link>
             </nav>
         )
     }
 }
+
+const ButtonContainer = styled.button`
+    text-transform : capitalize;
+    font-size : 1.4rem;
+    background : transparent;
+    border : 0.05rem solid var(--lightBlue);
+    color : var(--lightBlue);
+    border-radius : 0.5rem;
+    padding : 0.2rem 0.5rem;
+    cursor : pointer;
+    margin : 0.2rem 0.5rem 0.2rem 0;
+    transition : all 0.5s ease-in-and-out;
+    &:hover {
+        background : var(--lightBlue);
+        color : var(--mainBlue);
+    }
+`
