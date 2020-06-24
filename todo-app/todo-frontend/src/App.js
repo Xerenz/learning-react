2d1652136dc926147925e49e2521af7308c84dba
Xerenz <martingeo15@gmail.com>
2020-06-24 06:14:09 +0530

    some progress

--- todo-app/todo-frontend/src/App.js
+++ todo-app/todo-frontend/src/App.js
@@ -3,13 +3,39 @@ import React, { Component } from 'react';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 import CheckedList from './components/CheckedList';
+import { getTodoTasks, createTodoTask } from './APIHelper'
 
 export default class App extends Component {
+
+  state = {
+    tasks : []
+  }
+
+  componentDidMount() {
+    this.setTasks()
+  }
+
+  setTasks = async () => {
+    let todos = await getTodoTasks()
+    let tasks = []
+    console.log(todos)
+    todos.forEach(task => {
+      tasks = [...tasks, {...task}]
+    })
+    this.setState({ tasks })
+  }
+
+  addToTaskList = task => {
+    this.setState({
+      tasks : [...this.state.tasks, task]
+    })
+  }
+
   render() {
     return (
       <React.Fragment>
-        <TodoForm/>
-        <TodoList/>
+        <TodoForm method={ this.addToTaskList } />
+        <TodoList list={ this.state.tasks }/>
         <CheckedList/>
       </React.Fragment>
     )
--- todo-app/todo-frontend/src/components/TodoForm.js
+++ todo-app/todo-frontend/src/components/TodoForm.js
@@ -1,36 +1,34 @@
 import React, { Component } from 'react';
 
-import { createTodoTask } from "../APIHelper";
+import { createTodoTask } from '../APIHelper'
 
 export default class TodoForm extends Component {
 
     state = {
-        value : ""
-    };
+        box : ''
+    }
 
-    handleChange = event => {
-        this.setState({
-            value : event.target.value
-        });
+    handleInput = event => {
+        this.setState({ box : event.target.value })
     }
 
     handleSubmit = async event => {
         event.preventDefault();
-        const createdTask = await createTodoTask(this.state.value);
-        console.log(createdTask)
-        this.setState({value : ""})
-    }
+        let newTask = await createTodoTask(this.state.box)
+        this.props.method(newTask)
+    }   
 
     render() {
+
         return (
             <div className="container">
                 <form onSubmit={ this.handleSubmit }>
                     <div className="form-row my-5">
                         <div className="col-10">
                             <input type="text" 
-                            value={ this.state.value } 
+                            value={ this.state.box }
                             className="form-control" 
-                            onChange={ this.handleChange } />
+                            onChange={ this.handleInput } />
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-warning">Add</button>
